db07e43a275287f106cdcb28bf66f7ffcae90942
tangtang <gherson7827123@gmail.com>
2018-08-08 15:43:07 -0400

    Update pandasforwork.py

--- pandasforwork.py
+++ pandasforwork.py
@@ -1,1 +1,18 @@
 
+import pandas as pd 
+import numpy as np 
+
+df = pd.read_csv("cheatsheet.csv", encoding = "ISO-8859-1",na_filter=False)
+
+df = pd.DataFrame(df)
+
+list1=[85920333,95920028,90909021,80909935]
+
+df.loc[df['GROUPER'].isin(list1)," Ownership"] = 'No Ownership'
+df.loc[df['GROUPER'].isin([90111007]),'Tax Id #'] = '901034858'
+df.loc[df['GROUPER'].isin([90111007]),'Effective Date of Original Contract'] = '1.1.2011'
+df.loc[df['GROUPER'].isin([90111007]),'Effective Date of Grouper'] = '1.1.2012'
+
+writer = pd.ExcelWriter('output.xlsx')
+df.to_excel(writer,'Sheet1')
+writer.save()
